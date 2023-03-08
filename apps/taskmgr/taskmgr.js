eda700fee4671eef9a9225f070fe838dd72b36d1
Kousuke Kawahira <kawahira@binzume.net>
2023-03-09 07:16:13 +0900

    taskmgr: auto refresh

--- apps/taskmgr/taskmgr.js
+++ apps/taskmgr/taskmgr.js
@@ -8,12 +8,12 @@ AFRAME.registerComponent('task-manager', {
 			this._initRunningAppList();
 		}, { once: true });
 	},
+	remove() {
+		clearInterval(this._updateTimer);
+	},
 	_initRunningAppList() {
 		this._elByName('kill-all-button').addEventListener('click', (ev) => {
 			this._appManager.killAll();
-		});
-		this._elByName('refresh-button').addEventListener('click', (ev) => {
-			this._updateRunningAppList();
 		});
 		this.listEl = this._elByName('running-app-list');
 		let listEl = this.listEl;
@@ -21,18 +21,20 @@ AFRAME.registerComponent('task-manager', {
 		let self = this;
 		list.setAdapter({
 			create() {
-				let el = document.createElement('a-xylabel');
-				el.setAttribute('width', 4.5);
+				let xyrect = self.el.components.xyrect;
+				let containerWidth = xyrect.width;
+				let el = document.createElement('a-xybutton');
+				el.setAttribute('width', containerWidth - 1);
 				el.setAttribute('height', 0.4);
 				el.setAttribute('xyrect', {});
-				el.setAttribute('xylabel', { wrapCount: 16, renderingMode: 'canvas' });
+				el.setAttribute('xylabel', { wrapCount: 28, align: 'left', renderingMode: 'canvas' });
 				let unloadButton = document.createElement('a-xybutton');
 				unloadButton.setAttribute('width', 0.3);
 				unloadButton.setAttribute('height', 0.3);
-				unloadButton.setAttribute('position', { x: 3.0, y: 0, z: 0.05 });
+				unloadButton.setAttribute('position', { x: (containerWidth / 2) - 0.2, y: 0, z: 0.05 });
 				unloadButton.setAttribute('label', "X");
 				unloadButton.addEventListener('click', (ev) => {
-					let app = self.runnings[el.dataset.index];
+					let app = self._runnings[el.dataset.index];
 					app.el.parentNode.removeChild(app.el);
 					self._updateRunningAppList();
 				});
@@ -46,21 +48,25 @@ AFRAME.registerComponent('task-manager', {
 			}
 		});
 		listEl.addEventListener('clickitem', (ev) => {
-			this.runnings[ev.detail.index].el.focus();
+			this._runnings[ev.detail.index].el.focus();
 		});
 
 		this._updateRunningAppList();
+		this._updateTimer = setInterval(() => this._updateRunningAppList(), 1000);
 	},
 	_updateRunningAppList() {
 		let apps = [];
 		for (let el of this._appManager.getRunningApps()) {
 			let name = el.tagName;
+			if (el.components.vrapp && el.components.vrapp.app) {
+				name = el.components.vrapp.app.name;
+			}
 			if (el.components.xywindow) {
-				name = el.components.xywindow.data.title;
+				name += ": " + el.components.xywindow.data.title;
 			}
 			apps.push({ el: el, name: name });
 		}
-		this.runnings = apps;
+		this._runnings = apps;
 		this.listEl.components.xylist.setContents(apps);
 	},
 	/**
